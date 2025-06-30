d136f5f914e6d4657bb7382d0a02e29f4c093a25
suriya911 <suriyachellappan@gmail.com>
2025-06-30 00:28:02 -0700

    test cases

---
+++ jest.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    testEnvironment: 'jsdom',
+    setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
+    moduleNameMapper: {
+        '^@/(.*)$': '<rootDir>/src/$1',
+        '\\.(css|less|scss|sass)$': 'identity-obj-proxy',
+    },
+    transform: {
+        '^.+\\.(ts|tsx)$': 'ts-jest',
+    },
+    testPathIgnorePatterns: ['/node_modules/', '/.next/'],
+}; 
